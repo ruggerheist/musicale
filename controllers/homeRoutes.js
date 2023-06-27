cf0be861fe1834123aa04081c7008f852bd96d5b
Rick Heist <rick.heist@gmail.com>
2023-06-26 22:33:28 -0400

    main api is not cooperating currently

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const ticketmaster = require("../utils/ticketmaster-api.js");
+// const ticketmaster = require("../utils/ticketmaster-api.js");
 
 router.get("/", async (req, res) => {
   try {
@@ -19,19 +19,30 @@ router.get("/calendar", async (req, res) => {
 
 router.get('/searchcity', async (req, res) => {
   try {
-    const response = await fetch(data._embedded.events[i]);
-    const eventDate = response.date;
-    const eventName = response.name;
-    const eventVenue = response.venue[i].name;
-    const eventCity = response.city;
-    
-    // const eventImage = response.images[i]
-    // const sales = response.sales;
-    // let searchData = response.json()
-    res.render('searchcity', {searchData});
+    const apikey = process.env.API_KEY;
+    const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=358&apikey=${apikey}`
+    const response = await fetch(apiUrl);
+    const searchData = await response.json();
+    console.log(response);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+    
+    // const eventTime = response.dates;
+
+    // const eventDate = response.dates;
+    // const eventName = response.name;
+    // const eventVenue = response.venue[i].name;
+    // const eventCity = response.city;
+    
+    // const eventImage = response.images[i]
+    // const sales = response.sales;
+    // let searchData = response.json()
+    
+
+    
+
+
 
 module.exports = router;
