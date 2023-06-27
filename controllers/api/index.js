a00468cb5829c36f58c138b3c25598825c37ddf0
santi <santi.arroyo551@gmail.com>
2023-06-27 12:24:37 -0400

    adds comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 router.post('/signup', (req, res) => {
+    //TODO: query the database to insert new user using req.body
     console.log('sign up',req.body)
     res.redirect('/')
 })
