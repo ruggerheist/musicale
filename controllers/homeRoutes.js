815a6fe5c731942efa4dbc947949c6ecd729ea48
Rick Heist <rick.heist@gmail.com>
2023-06-26 21:48:07 -0400

    added api routes, utils, and models

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const ticketmaster = require("../utils/ticketmaster-api.js");
 
 router.get("/", async (req, res) => {
   try {
@@ -11,6 +12,23 @@ router.get("/", async (req, res) => {
 router.get("/calendar", async (req, res) => {
   try {
     res.render("calendar");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/searchcity', async (req, res) => {
+  try {
+    const response = await fetch(data._embedded.events[i]);
+    const eventDate = response.date;
+    const eventName = response.name;
+    const eventVenue = response.venue[i].name;
+    const eventCity = response.city;
+    
+    // const eventImage = response.images[i]
+    // const sales = response.sales;
+    // let searchData = response.json()
+    res.render('searchcity', {searchData});
   } catch (err) {
     res.status(500).json(err);
   }
