580e74ebedf9189a2a0124c815dfd32b7b0ec401
Nicole Bullock <nicolebullock@nicoles-air.myfiosgateway.com>
2023-06-30 13:59:52 -0400

    fixed models to reflect a many to many relationship

--- models/index.js
+++ models/index.js
@@ -1,15 +1,19 @@
 const User = require("./User");
 const Concert = require("./Concert");
-Concert
-User.hasMany(Concert, {
+const UserConcert = require("./UserConcert");
+
+// Users belong to many concerts
+User.belongsToMany(Concert, {
+    through: UserConcert,
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
 });
 
-Concert.belongsTo(User, {
-    foreignKey: 'user_id',
+// Concerts belong to many Users 
+Concert.belongsToMany(User, {
+    through: UserConcert,
+    foreignKey: 'concert_id'
 });
 
 //export evrything from index.js
 
-module.exports = { User, Concert }
+module.exports = { User, Concert, UserConcert }
