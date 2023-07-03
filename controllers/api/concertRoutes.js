9734f47c1b20104515b81a7cc3121c4b722bea15
Nicole Bullock <nicolebullock@Nicoles-Air.fios-router.home>
2023-07-03 19:56:53 -0400

    worked on get routes to get calendar to render saved concerts

--- controllers/api/concertRoutes.js
+++ controllers/api/concertRoutes.js
@@ -4,27 +4,24 @@ const withAuth = require('../../utils/auth');
 
 //The /api/concerts endpoint
 
-
-// , withAuth,
-
-// router.get('/', withAuth, async (req, res) => {
-//     try {
-//         const concertData = await Concert.findAll({
-//         where: {
-//             user_id: req.session.user_id
-//         }
-//         });
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const concertData = await Concert.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+        });
     
-//         const concerts = concertData.map((concert) => concert.get({ plain: true }));
+        const concerts = concertData.map((concert) => concert.get({ plain: true }));
     
-//         res.render('concerts', {
-//         concerts,
-//         logged_in: req.session.logged_in
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+        res.render('concerts', {
+        concerts,
+        logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.post('/', withAuth, async (req, res) => {
     try {
